e208b68f5f793d25df52b5c31032201ffb188f9f
xnorpx <xnorpx@users.noreply.github.com>
2026-09-05 14:26:50 +0000

    deploy: 9829c709fac7d708f1ae78ed14d271c34994d663

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f90f5cf2.js',
+                path_to_root + 'searchindex-038c8a66.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
