961caff4aa371436f5382c48616b2aa440cd8e33
Marcus Asteborg <maastebo@microsoft.com>
2026-09-08 17:53:11 -0700

    fix: protect Windows recording maintenance fixtures

---
+++ .github/scripts/protect-test-directory.ps1
@@ -0,0 +1,23 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$Directory
+)
+
+$ErrorActionPreference = 'Stop'
+$owner = [System.Security.Principal.WindowsIdentity]::GetCurrent().User
+$security = [System.Security.AccessControl.DirectorySecurity]::new()
+$security.SetOwner($owner)
+$security.SetAccessRuleProtection($true, $false)
+
+foreach ($identity in @($owner.Value, 'S-1-5-18', 'S-1-5-32-544')) {
+    $rule = [System.Security.AccessControl.FileSystemAccessRule]::new(
+        [System.Security.Principal.SecurityIdentifier]::new($identity),
+        [System.Security.AccessControl.FileSystemRights]::FullControl,
+        [System.Security.AccessControl.InheritanceFlags]'ContainerInherit, ObjectInherit',
+        [System.Security.AccessControl.PropagationFlags]::None,
+        [System.Security.AccessControl.AccessControlType]::Allow
+    )
+    $security.AddAccessRule($rule)
+}
+
+Set-Acl -LiteralPath $Directory -AclObject $security
